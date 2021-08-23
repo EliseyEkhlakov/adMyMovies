4824cac454fa6041bc74cea07d718684888d23ff
Елисей Ехлаков <elisey.ekhlakov@krit.pro>
2021-08-23 13:52:55 +0400

    Initial commit

---
+++ app/src/main/java/com/demo/mymovies/MainActivity.java
@@ -0,0 +1,31 @@
+package com.demo.mymovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.demo.mymovies.data.Movie;
+import com.demo.mymovies.utils.JSONUtils;
+import com.demo.mymovies.utils.NetworkUtils;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 5);
+        ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
+        StringBuilder builder = new StringBuilder();
+        for(Movie movie: movies){
+            builder.append(movie.getTitle()).append("\n");
+        }
+        Log.i("MyMovie", builder.toString());
+    }
+}
---
+++ app/src/main/java/com/demo/mymovies/utils/JSONUtils.java
@@ -0,0 +1,51 @@
+package com.demo.mymovies.utils;
+
+import com.demo.mymovies.data.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class JSONUtils {
+
+    private static final String KEY_RESULTS = "results";
+    private static final String KEY_VOTE_COUNT = "vote_count";
+    private static final String KEY_ID = "id";
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_ORIGINAL_TITLE = "original_title";
+    private static final String KEY_OVERVIEW = "overview";
+    private static final String KEY_POSTER_PATH = "poster_path";
+    private static final String KEY_BACKDROP_PATH = "backdrop+path";
+    private static final String KEY_VOTE_AVERAGE = "vote_average";
+    private static final String KEY_RELEASE_DATE = "release_date";
+
+    public static ArrayList<Movie> getMoviesFromJSON(JSONObject jsonObject) {
+        ArrayList<Movie> result = new ArrayList<>();
+        if (jsonObject == null) {
+            return result;
+        }
+        try {
+            JSONArray jsonArray = jsonObject.getJSONArray(KEY_RESULTS);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject objectMoview = jsonArray.getJSONObject(i);
+                int id = objectMoview.getInt(KEY_ID);
+                int voteCount = objectMoview.getInt(KEY_VOTE_COUNT);
+                String title = objectMoview.getString(KEY_TITLE);
+                String originalTitle = objectMoview.getString(KEY_ORIGINAL_TITLE);
+                String overview = objectMoview.getString(KEY_OVERVIEW);
+                String posterPath = objectMoview.getString(KEY_POSTER_PATH);
+                String backdropPath = objectMoview.getString(KEY_BACKDROP_PATH);
+                double voteAverage = objectMoview.getDouble(KEY_VOTE_AVERAGE);
+                String releaseDate = objectMoview.getString(KEY_RELEASE_DATE);
+                Movie movie = new Movie(id, voteCount, title, originalTitle, overview, posterPath, backdropPath, voteAverage, releaseDate);
+                result.add(movie);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
