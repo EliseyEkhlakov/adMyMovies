d618d30d28800bc566e2a6096424e8eb3a070f48
Елисей Ехлаков <elisey.ekhlakov@krit.pro>
2021-08-24 17:42:37 +0400

    +Adapter

---
+++ app/src/main/java/com/demo/mymovies/MovieAdapter.java
@@ -0,0 +1,65 @@
+package com.demo.mymovies;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.demo.mymovies.data.Movie;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    private ArrayList<Movie> movies;
+
+    public MovieAdapter() {
+        movies = new ArrayList<>();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position, @NonNull List<Object> payloads) {
+        super.onBindViewHolder(holder, position, payloads);
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_item, parent, false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView imageViewSmallPoster;
+
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageViewSmallPoster = itemView.findViewById(R.id.imageViewSmallPoster);
+        }
+    }
+
+    public void setMovies(ArrayList<Movie> movies) {
+        this.movies = movies;
+        notifyDataSetChanged();
+    }
+
+    public void addMovies(ArrayList<Movie> movies) {
+        this.movies.addAll(movies);
+        notifyDataSetChanged();
+    }
+
+    public ArrayList<Movie> getMovies() {
+        return movies;
+    }
+}
